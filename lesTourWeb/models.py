586bb4ad3c8f4ca2258992d9d6f455079a898c44
MarcosE24 <42250217+MarcosE24@users.noreply.github.com>
2023-09-30 05:13:02 -0400

    Creation of the "Reserva" model

--- lesTourWeb/models.py
+++ lesTourWeb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Reserva(models.Model):
+    id_user= models.ForeignKey(User, on_delete=models.CASCADE)
+    checkin_datetime= models.DateTimeField()
+    checkout_datetime= models.DateTimeField()
+    total_cost= models.IntegerField(default=0)
+    request_datetime= models.DateTimeField(auto_now_add=True)
+    id_room= models.IntegerField(default=0)
+    observation= models.TextField(blank=True, max_length=500)
