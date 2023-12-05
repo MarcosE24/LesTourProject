5e4adc722bb4935552124c2fc26cafca044b8cf6
maurogimenezb <maurogimenezb@gmail.com>
2023-12-05 11:49:49 -0300

    Unit tests for view URLs

--- lesTourApp/tests.py
+++ lesTourApp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestUrls(TestCase):
+    def test_url_home(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_signup(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_reservation(self):
+        url = reverse('reservation')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_signin(self):
+        url = reverse('signin')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
